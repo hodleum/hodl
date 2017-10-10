3e3819cf06053e4dd0d680faa4fda0f2e55a27e8
Timur Kudashev <lemur1416@gmail.com>
2017-10-10 18:05:07 +0300

    Теперь мы можем отправлять почтовых голубей и жалких рабов

--- net.py
+++ net.py
@@ -2,14 +2,21 @@ import block
 from socket import socket
 
 
-def listen():    # слушает, если что-то пришло, возвращает tuple (IP откуда пришло, адрес в системе того устройства, сообщение)
-    s = socket()
+port = 6666
+sock_send = socket()
+sock_listen = socket()
 
-def send(myaddr, ip, mess):    # отправляет сообщение на адрес ip, ничего не возвращает
-    pass
+def listen():    # слушает, если что-то пришло, возвращает tuple (IP откуда пришло, адрес в системе того устройства, сообщение)
+
+
+def send(ip, mess, myaddr=''):    # отправляет сообщение на адрес ip, ничего не возвращает
+    sock_send.connect((ip, port))
+    sock_send.send(myaddr + "  " + mess)
+
 
 def is_my_ip_white():    # возвращает True/False белый ли IP устройства, на котором выполняется программа
     pass
+
 
 def is_ip_white(ip):    # возвращает True/False, белый ли ip
     pass
