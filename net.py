c3ed47388a1249a3dc8d9e9f33322b5b32db093b
Timur Kudashev <lemur1416@gmail.com>
2017-10-10 23:27:32 +0300

    Как определить IP отправителя?

--- net.py
+++ net.py
@@ -7,11 +7,25 @@ sock_send = socket()
 sock_listen = socket()
 
 def listen():    # слушает, если что-то пришло, возвращает tuple (IP откуда пришло, адрес в системе того устройства, сообщение)
+	sock.listen(1)
+	conn, addr = sock.accept()
+	data = conn.recv(4096)
+	mess = ''
+	addr = ''
+	ip = ''
+	flag = false
+	for i in data :
+		if flag == True :
+			mess += i
+		else :
+			addr += i
+	summary_tuple = (ip, addr, mess)
+	return summary_tuple
 
 
 def send(ip, mess, myaddr=''):    # отправляет сообщение на адрес ip, ничего не возвращает
     sock_send.connect((ip, port))
-    sock_send.send(myaddr + "  " + mess)
+    sock_send.send(myaddr + " " + mess)
 
 
 def is_my_ip_white():    # возвращает True/False белый ли IP устройства, на котором выполняется программа
