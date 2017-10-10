daa358943548ede2d8632b7757aac476da5cec2f
leofnch <leva.chizhow@gmail.com>
2017-10-10 22:17:02 +0300

    Обнаружено, что cryptogr.py работает для подписей примерно никак, решено переписать, написаны описания функций

--- cryptogr.py
+++ cryptogr.py
@@ -7,17 +7,17 @@ from base64 import b64encode, b64decode
 
 
 global f
-f=['', '']
+f = ['priv', 'pub']
 
-def h(s):
+def h(s):    # Хэш, МЕНЯТЬ НИНАДА
     return MD5.new(bytes(str(s), 'utf-8')).digest()
 
 
-def keys():
+def keys():    # возвращает ключи, примерно правильно
     return RSA.importKey(open(f[0], "r").read()).exportKey(), RSA.importKey(open(f[1], "r").read()).exportKey()
 
 
-def genkeys():
+def genkeys():  # генерирует приватный и открытый ключи, пишет их в файлы f[0] и f[1]
     keys = RSA.generate(1024)
     f1 = open(f[0], 'wb')
     f2 = open(f[1], 'wb')
@@ -25,10 +25,13 @@ def genkeys():
     f2.write(keys.publickey().exportKey())  # write public key
 
 
-def sign(data, key='my key'):
-    if key=='my key':
+def sign(data, key='my key'):    # подписывает data (тип str) секретным ключом key (тип str).
+    # Если key == 'my key', подписывает ключом из f[0]
+    # Возвращает подпись (тип str)
+    # ВАЖНО: verify_str должна нормально работать с подписью, которую дает эта функция
+    if key =='my key':
         key = open(f[0], "r").read()
-    data=bytes(data, 'utf-8')
+    data = bytes(data, 'utf-8')
     rsakey = RSA.importKey(key)
     signer = PKCS1_v1_5.new(rsakey)
     digest = SHA256.new()
@@ -38,7 +41,7 @@ def sign(data, key='my key'):
     return b64encode(sign)
 
 
-def verify_sign(signature, data, pub_key):
+def verify_sign(signature, data, pub_key):  # проверяет, подписал ли pub_key(тип str) data(тип str) и получил signature
     try:
         rsakey = RSA.importKey(pub_key)
         signer = PKCS1_v1_5.new(rsakey)
