ce62d89cedda83032fae890569eef2d4ec7d7993
leofnch <leva.chizhow@gmail.com>
2017-11-02 23:13:37 +0300

    Encoding to str is now using JSON, docstrings wrote, fixes

--- block.py
+++ block.py
@@ -1,64 +1,68 @@
 import cryptogr as cg
 import time
 from itertools import chain
-
+import json
 
 minerfee = 1
 txs_in_block = 50
 maxblocksize = 40000
+
+
 # todo: ТЕСТЫ! ОЧЕНЬ НАДО ПОТЕСТИТЬ! (для этого надо НАКОНЕЦ ДОПИСАТЬ cryptogr.py) Файл практически не тестился.
 class Blockchain(list):
     """Class for blockchain"""
-    def money(self, wallet):    # проверяет, сколько денег у wallet
+
+    def money(self, wallet):
+        """Counts money on wallet"""
         money = 0
-        for block in self:   # перебираем все транзакции в каждом блоке
+        for block in self:  # перебираем все транзакции в каждом блоке
             for txs in block.txs:
                 if wallet in txs.outs and txs.is_open(self):
                     money += txs.outns[txs.outs.index(wallet)]
         return money
 
     def new_block(self, n, creator, txs=[]):
-        """создает новый блок и сразу же добавляет его в цепочку."""
+        """Creates the new block and adds it to chain"""
         self.append(Block(n, creator, self, txs))
 
     def is_valid(self):
-        """проверяет валидность всей цепочки"""
+        """Returns validness of the whole chain"""
         for b in self:
             if not b.is_valid(self):
                 return False
         return True
 
-    def new_transaction(self, author, froms, outs, outns, sign = 'signing', privkey = 'me'):
+    def new_transaction(self, author, froms, outs, outns, sign='signing', privkey=''):
+        """Creates new transaction and adds it to the chain"""
         tnx = Transaction()
         for i, block in enumerate(self[1:]):
             if not block.is_full():
                 tnx.gen(author, froms, outs, outns, (i, len(block.txs)), sign, privkey)
-                block.append(tnx, self)
+                block.append(tnx)
                 break
 
     def __str__(self):
-        s = ''
-        for block in self:
-            s += 'д' + str(block)
+        """Encodes blockchain to str"""
+        return json.dumps([str(e) for e in self])
 
-    def fromstr(self, s):
-        self = []
-        s = s.split('д')
-        for b in s:
+    def from_json(self, s):
+        """Decodes blockchain from str"""
+        bs = json.loads(s)
+        for b in bs:
             block = Block()
-            block.fromstr(b)
+            block.from_json(b)
             self.append(block)
 
     def new_sc(self, text, author, needsinf=False, payment_method='for execution', payment_opts={'for 1 execution': 1}):
+        """creates new smart contract and adds it to the chain"""
         for i, block in enumerate(self):
             if not block.is_full():
                 block.contracts.append(text, author, (i, len(block.contracts)), needsinf, payment_method, payment_opts)
                 break
 
-    def __eq__(self, other) :
-        dict1 = self.__dict__()
-        dict2 = other.__dict__()
-        return cmp(dict1, dict2)
+    def __eq__(self, other):
+        """equal function"""
+        return self.__dict__ == other.__dict__
 
 
 class Block:
@@ -78,99 +82,93 @@ class Block:
         self.update()
 
     def __str__(self):
-        s = ''
-        for t in self.txs:
-            s += str(t) + 'б'
-        s = s[:-1]
-        s += 'г' + str(self.n) + 'б' + str(self.timestamp) + 'б' + str(self.prevhash) + 'б' + str(self.creator)
-        s += 'г'
-        for c in self.contracts:
-            s += str(c) + 'б'
-        return s
+        """Encodes block to str using JSON"""
+        return json.dumps(([str(t) for t in self.txs], self.n, self.timestamp, self.prevhash, self.creator,
+                           [str(c) for c in self.contracts]))
 
-    def fromstr(self, s):
-        s = s.split('г')
-        txs = s[0].split('б')
-        self.txs = []
-        for t in txs:
+    def from_json(self, s):
+        """Decodes block from str using JSON"""
+        s = json.loads(s)
+        for t in s[0]:
             tnx = Transaction()
-            tnx.fromstr(t)
+            tnx.from_json(t)
             self.txs.append(tnx)
-        scs = s[2].split('б')
-        self.contracts = []
-        for sc in scs[:-1]:
-            contract = Smart_contract('', '', [0,0])
-            contract.fromstr(sc)
-            self.contracts.append(contract)
-        pars = s[1].split('б')
-        self.n, self.timestamp, self.prevhash, self.creator = int(pars[0]), float(pars[1]), str(pars[2]), str(pars[3])
+        for c in s[5]:
+            sc = Smart_contract()
+            sc.from_json(c)
+            self.contracts.append(sc)
+        self.n, self.timestamp, self.prevhash, self.creator = s[1], s[2], s[3], s[4]
         self.update()
 
-    def append(self, txn, bch):    # функция для добавления транзакции в блок
-        self.txs.append(txn)    # добавляем транзакцию в список транзакций
-        self.update()    # обновляем хэш
+    def append(self, txn):
+        """Adds txn to block"""
+        self.txs.append(txn)  # добавляем транзакцию в список транзакций
+        self.update()  # обновляем хэш
 
-    def update(self):    # обновляет хэш
-        h = ''.join([str(self.prevhash), str(self.timestamp), str(self.n)]+[str(t.hash) for t in self.txs])
+    def update(self):
+        """Updates hash"""
+        h = ''.join([str(self.prevhash), str(self.timestamp), str(self.n)] + [str(t.hash) for t in self.txs])
         self.h = cg.h(str(h))
 
-    def is_valid(self, bch):    # проверка валидности каждой транзакции блока и соответствия хэша
+    def is_valid(self, bch):
+        """Returns validness of block"""
         h = str(bch.index(self)) + str(self.prevhash) + str(self.timestamp) + str(self.n)
         if self.txs[0].froms != [['nothing']] or self.txs[0].author != 'mining' \
-                or self.txs[0].outs != [self.creator]\
+                or self.txs[0].outs != [self.creator] \
                 or self.txs[0].outns != minerfee:
             return False
         for t in self.txs[1:]:
             h = h + str(t.hash)
             if not t.is_valid(bch):
                 return False
-        v = cg.h(str(h)) == self.h and self.prevhash == bch[bch.index(self)-1].h
+        v = cg.h(str(h)) == self.h and self.prevhash == bch[bch.index(self) - 1].h
         return v
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
     def is_full(self):
+        """is block full"""
         return len(str(self)) >= maxblocksize
 
+
 class Transaction:
-    """Class for transactions"""
+    """Class for transaction"""
     # форма для передачи транзакций строкой(разделитель - русское а):
     # author + а + str(froms)+ а + str(outs) + а + str(outns) + а + str(time)+ а + sign
-    def __str__(self):    # преобразование в строку, которая может быть расшифрована функцией fromstr
-        return self.author + 'а'+str(self.froms) + 'а' + str(self.outs) + 'а' + str(self.outns) + 'а' \
-                + str(self.index) \
-                + 'а' + str(self.sign)
-    # todo: заменить fromstr на from_json
-    def fromstr(self, s):   # Обратная функция tostr
-        l = s.split('а')
-        self.gen(l[0], eval(l[1]), eval(l[2]), eval(l[3]), eval(l[4]), l[5])
+    def __str__(self):
+        """Encodes transaction to str using JSON"""
+        return json.dumps((self.author, self.froms, self.outs, self.outns, self.index, self.sign))
 
-    def gen(self, author, froms, outs, outns, index, sign='signing', privkey='me'):
+    def from_json(self, s):
+        """Decodes transacion from str using JSON"""
+        self.gen(*json.loads(s))
+
+    def gen(self, author, froms, outs, outns, index, sign='signing', privkey=''):
         self.froms = froms  # номера транзакций([номер блока в котором лежит нужная транзакция,
-                               # номер нужной транзакции в блоке),
-                               # из которых эта берет деньги
-        self.outs = outs    # номера кошельков-адресатов
+        # номер нужной транзакции в блоке),
+        # из которых эта берет деньги
+        self.outs = outs  # номера кошельков-адресатов
         self.outns = outns  # количество денег на каждый кошелек-адресат
         self.author = author  # тот, кто проводит транзакцию
         self.index = index
-        if sign == 'signing':    # транзакция может быть уже подписана,
-                               # или может создаваться новая транзакция с помощью Transaction().
-                               # Соответственно может быть нужна новая подпись.
-            if privkey == 'me':     # Подписываем транзакцию тем ключом, который сохранен на этом компьютере как основной
-                self.sign = cg.sign(str(self.froms) + str(self.outs) + str(self.outns))
-            else:    # Или кастомным
-                self.sign = cg.sign(str(self.froms) + str(self.outs) + str(self.outns),
-                                       privkey)
-        else:    # Если транзакция не проводится, а создается заново после передачи, то подпись уже известна
+        if sign == 'signing':  # транзакция может быть уже подписана,
+            # или может создаваться новая транзакция с помощью Transaction().
+            # Соответственно может быть нужна новая подпись.
+            self.sign = cg.sign(str(self.froms) + str(self.outs) + str(self.outns), privkey)
+        else:  # Если транзакция не проводится, а создается заново после передачи, то подпись уже известна
             self.sign = sign
         x = ''.join(chain([str(self.sign), str(self.author), str(self.index)], [str(f) for f in self.froms],
                           [str(f) for f in self.outs], [str(f) for f in self.outns]))
         self.hash = cg.h(str(x))
 
-    def is_valid(self, bch):    # Проверка наличия требуемых денег
-                                # в транзакциях, из которых берутся деньги и соответствия подписи и хэша
-                                # Проверка соответствия подписи
+    def is_valid(self, bch):
+        """Returns validness of transaction.
+        Checks:
+        is sign valid
+        are all money spent"""
         if not self.author[0:2] == 'sc':
-            if not cg.verify_sign(self.sign, str(self.froms) + str(self.outs) +
-                    str(self.outns), self.author):
+            if not cg.verify_sign(self.sign, str(self.froms) + str(self.outs) + str(self.outns), self.author):
                 print(self.index, 'is not valid: sign is wrong')
                 return False
         else:
@@ -180,7 +178,7 @@ class Transaction:
                 tnx_needed, tnx_created, froms, outs, outns = sc.exec()[1:]
                 if tnx_needed:
                     selfind = froms.index(self.froms)
-                    if not (tnx_created and outs[selfind] == self.outs and outns[selfind]==self.outns):
+                    if not (tnx_created and outs[selfind] == self.outs and outns[selfind] == self.outns):
                         return False
                 else:
                     print(self.index, 'is not valid: sc')
@@ -189,7 +187,7 @@ class Transaction:
                 print(self.index, 'is not valid: exception183')
                 return False
         inp = 0
-        for t in self.froms:    # Проверка наличия требуемых денег в транзакциях-донорах
+        for t in self.froms:  # Проверка наличия требуемых денег в транзакциях-донорах
             try:
                 if t == ['nothing']:
                     if not (self.index[1] == 0 and self.outs[0] == bch[self.index[0]].creator):
@@ -206,7 +204,7 @@ class Transaction:
                     inp = inp + txs.outns[txs.outs.index(self.author)]
             except:
                 print(self.index, 'is not valid: exception197')
-                return False    # Если возникает какая-нибудь ошибка, то транзакция точно невалидная
+                return False  # Если возникает какая-нибудь ошибка, то транзакция точно невалидная
         o = 0
         for n in self.outns:  # должны быть израсходованы все взятые деньги
             o = o + n
@@ -214,27 +212,28 @@ class Transaction:
             print(self.index, 'is not valid: not all money')
             return False
         x = ''.join(chain([str(self.sign), str(self.author), str(self.index)], [str(f) for f in self.froms],
-                        [str(f) for f in self.outs], [str(f) for f in self.outns]))
+                          [str(f) for f in self.outs], [str(f) for f in self.outns]))
         if not self.hash == cg.h(str(x)):
             print(self.index, 'is not valid: hash is not valid')
             return False
         return True
 
-    def is_open(self, bch):  # Проверяет, не является ли эта транзакция чьим-то донором
-        for block in bch:   # перебираем все транзакции в каждом блоке
+    def is_open(self, bch):
+        """Is transaction don't used by other transaction"""
+        for block in bch:  # перебираем все транзакции в каждом блоке
             for txs in block.txs:
                 if self.index in txs.froms:
                     return False
         return True
-    def __eq__(self, other) :
-        dict1 = self.__dict__()
-        dict2 = other.__dict__()
-        return cmp(dict1, dict2)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
 
 class Smart_contract:
-    """Class for smart contracts"""
     # todo: дописать Smart_contract: добавить ограничения
-    def __init__(self, text, author, index, needsinf=False, payment_method = 'for execution', payment_opts={'for 1 execution' : 1}):
+    def __init__(self, text, author, index, needsinf=False, payment_method='for execution',
+                 payment_opts={'for 1 execution': 1}):
         self.text = text
         self.author = author
         self.payment_method = payment_method
@@ -242,35 +241,40 @@ class Smart_contract:
         self.result = ''
         self.needsinf = needsinf
         self.payment_opts = payment_opts
+        self.info = ''
 
     def execute(self, bch, inf=''):
+        """smart contract's execution"""
         loc = {}
+        loc['info'] = self.info
         if self.needsinf:
             exec(self.text, {'inf': str(inf)}, loc)
         else:
             exec(self.text, {}, loc)
         result = loc['result']
-        tnx_needed = loc['tnx_needed']    # смарт-контракт может проводить транзакции от своего имени
+        tnx_needed = loc['tnx_needed']  # смарт-контракт может проводить транзакции от своего имени
         tnx_created = loc['tnx_created']
         froms = loc['froms']
         outs = loc['outs']
         outns = loc['outns']
-        sc_needed = loc['sc_needed']    # смарт-контракт может создавать смарт-контракты
+        sc_needed = loc['sc_needed']  # смарт-контракт может создавать смарт-контракты
         sc_created = loc['sc_created']
         sc_text = loc['sc_text']
         sc_author = loc['sc_author']
         sc_payment_method = loc['sc_payment_method']
         sc_needsinf = loc['sc_needsinf']
         sc_payment_opts = loc['sc_payment_opts']
+        self.info = str(loc['info'])
         # Смарт-контракт изменяет переменные froms, outs и outns на параметры совершаемой
         # им транзакции. Если ему не надо совершать транзакцию, он их не изменяет. Также он может возвращать result.
         # класс result - str
-        if not self.needsinf:   # контракты, которые принимают входную информацию не могут создавать другие контракты и транзакции
+        if not self.needsinf:  # контракты, которые принимают входную информацию не могут создавать другие контракты и транзакции
             if tnx_needed:
                 for i in range(len(froms)):
                     if not tnx_created[i]:
                         try:
-                            bch.new_transaction('sc' + str(self.index[0]) + ';' + str(self.index[1]), froms[i], outs[i], outns[i],
+                            bch.new_transaction('sc' + str(self.index[0]) + ';' + str(self.index[1]), froms[i], outs[i],
+                                                outns[i],
                                                 'sc' + str(self.index[0]) + ';' + str(self.index[1]),
                                                 'sc' + str(self.index[0]) + ';' + str(self.index[1]))
                         except:
@@ -279,30 +283,20 @@ class Smart_contract:
                 for i in range(len(sc_created)):
                     if not sc_created[i]:
                         try:
-                            bch.new_sc(sc_text[i], sc_author[i], sc_needsinf[i], sc_payment_method[i], sc_payment_opts[i])
+                            bch.new_sc(sc_text[i], sc_author[i], sc_needsinf[i], sc_payment_method[i],
+                                       sc_payment_opts[i])
                         except:
                             pass
         self.result = result
         return result, tnx_needed, tnx_created, froms, outs, outns
 
     def __str__(self):
-        return str(self.text) + 'е' + str(self.author) + 'е' + str(self.index[0]) + ';' + str(self.index[1]) + 'e' + \
-             str(self.needsinf) + 'е' + str(self.payment_method) + 'е' + str(self.payment_opts)
+        """Encodes contract to str"""
+        return json.dumps((self.text, self.author, self.index, self.needsinf, self.payment_method, self.payment_opts))
 
-    # from_json
-    @classmethod
-    def fromstr(cls, s):
-        # получить данные из строки
-        s = s.split('е')
-        text = s[0]
-        author = s[1]
-        index = [int(s[2].split(';')[0]), int(s[2].split(';')[1])]
-        needsinf = eval(s[3])
-        payment_method = s[4]
-        payment_opts = eval(s[5])
-        # собрать новый объект
-        return cls(text, author, index, needsinf, payment_method, payment_opts)
-    def __eq__(self, other) :
-        dict1 = self.__dict__()
-        dict2 = other.__dict__()
-        return cmp(dict1, dict2)
+    def from_json(self, s):
+        """Decodes contract from str"""
+        self.__init__(*json.loads(s))
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
