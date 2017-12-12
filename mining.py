4683cd14b3a9c24bf04f5cd8071c973beb3cf7f5
leofnch <leva.chizhow@gmail.com>
2017-12-12 18:06:43 +0300

    mining

--- mining.py
+++ mining.py
@@ -5,14 +5,14 @@ import cryptogr as cg
 
 #todo: write mining
 
-def pow_mine(bch, b):
+def pow_mining(bch, b):
     """proofs-of-work mining"""
     miners = bch[-1].powminers
-    miners.sort()
+    miners.sort(reversed=True)
     i = ((int(bch[-1].txs[-1].hash) + int(bch[-1].txs[-2].hash)) ** 2) % len(miners)
     while True:
         bl = block.Block(miners[i][1], [miners[i][2]], [0.4, 0.3, 0.3], bch, [], [], miners[i][3])
-        if bl.powhash < miners[i][0]:
+        if int(bl.powhash) < miners[i][0]:
             break
         else:
             i += 1
@@ -24,7 +24,7 @@ def pow_mine(bch, b):
     return b
 
 
-def pos_mine(b, bch):
+def pos_mining(b, bch):
     """Proofs-of-stake mining"""
     miners = []
     for tnx in bch[-1].txs:
@@ -37,23 +37,59 @@ def pos_mine(b, bch):
     b.update()
     return b
 
-def poc_mine(b, bch):
+def poc_mining(b, bch):
     """Proofs-of-capacity mining"""
     bindex = bch.index(b)
     miners = bch[bindex-1].pocminers
     miners.sort()
     i = ((int(bch[-1].txs[-1].hash) + int(bch[-1].txs[-4].hash)) ** 2) % len(miners)
     miner = miners[i]
+    y = [(int(bch[-1].txs[-1].hash) ** k) % miner[0] for k in range(1000)]
+    # miner[0] - number of values miner has calculated, miner[1] - miner's public key, miner[2] - xs miner has calculated
+    while v:
+        v = True
+        for k in range(1000):
+            if int(cg.h(str(miner[2][k] + len(bch)) + str(miner[1]))) == y[k]:
+                v = False
+                break
+        if v:
+            i += 1
+            miner = miners[i]
+            y = [(int(bch[-1].txs[-1].hash) ** i) % miner[0] for i in range(1000)]
     b.creators.append(miner[1])
     b.proportions.append(0.3)
     b.update()
     return b
 
+def poc_mine(n, bch, myaddr):
+    xs = []
+    f = open('poc_mine.txt', 'a')
+    y = [(int(bch[-1].txs[-1].hash) ** k) % n for k in range(1)]
+    for i in range(n):
+        f.write(cg.h(str(i+len(bch)) + str(myaddr)))
+    f.close()
+    file = open('poc_mine.txt', 'r')
+    f = [int(i) for i in list(file)]
+    file.close()
+    print(y)
+    for i in range(1):
+        try:
+            xs.append(f.index(y[i]))
+        except:
+            return xs, False
+    return xs, True
 
-def mine(b, bch):
-    # todo: write mining.mine()
-    pos_mine(b, bch)
-    return b
+
+def pow_mine(bch, nmax, myaddr):
+    n = 0
+    while True:
+        t = time.time()
+        bl = block.Block(n, [myaddr], [0.4, 0.3, 0.3], bch, [], [], t)
+        if int(bl.powhash) < nmax:
+            break
+        else:
+            n += 1
+    return n, t
 
 
 def validate(b, bch):
--- miningunittest.py
+++ miningunittest.py
@@ -1,11 +1,22 @@
 import mining
 import block
+import cryptogr as cg
 import unittest
 
 bch = block.Blockchain()
+my_keys = cg.gen_keys()
+your_pub_key = cg.gen_keys()[1]
+bch.new_block([my_keys[1], your_pub_key], [0.75, 0.25])
+bch.new_block([my_keys[1], your_pub_key], [0.85, 0.15])
+bch.new_transaction(my_keys[1], [(0, 0)], [your_pub_key, my_keys[1]], [0.5, 0.25], 'signing', my_keys[0])
 class MiningUnittest(unittest.TestCase):
-    def test_no_errors(self):
-        pass
+    def test_pow(self):
+        n, t = mining.pow_mine(bch, 3210400000000000000000000000000000000, my_keys[1])
+        print(n, t)
+
+    def test_poc(self):
+        xs = mining.poc_mine(10, bch, my_keys[1])
+        print(xs)
 
 
 if __name__ == '__main__':
