12d03db24e8cb619138a30ab3c0a94601e1a8a41
Timur Kudashev <lemur1416@gmail.com>
2018-02-27 18:09:39 +0300

    Started generating keys for functional tests

--- func_test_net.py
+++ func_test_net.py
@@ -1,8 +1,11 @@
+import sys
+import block
+import cryptogr as cg
+
+
 #Alice, Bob, Chuck, Dave are creating clear blockchain with genesis block
 #After that Alice creates transaction & sends it to Bob & Chuck(they all are waiting while net.py is doing it)
 #Two seconds later Bob creates block & sends it to Alice & Chuck
-
-import sys
-
+bch = block.Blockchain()
 
 name = sys.argv[0]
