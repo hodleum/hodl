270735da4122686f1984c868ad0a2c825fde268d
leofnch <leva.chizhow@gmail.com>
2018-03-10 14:54:55 +0300

    Net test

--- tests/func_test_net.py
+++ tests/func_test_net.py
@@ -1,22 +1,30 @@
-import sys
 import os
 import block
-import cryptogr as cg
-from Crypto.PublicKey import RSA
+import json
+import multiprocessing
+import net
+import time
 
 
 #Alice, Bob, Chuck, Dave are creating clear blockchain with genesis block
-#After that Alice creates transaction & sends it to Bob & Chuck(they all are waiting while net.py is doing it)
+#After that Alice creates transaction & waits for synchronization(other are waiting while net.py is doing it)
 #Two seconds later Bob creates block & sends it to Alice & Chuck
 
 
 bch = block.Blockchain()
 name = os.getenv('HODL_NAME')
-keys = [0, 1]
-f = open(name + "Priv.key", "r")
-keys[0] = RSA.importKey(f.read())
-f.close()
-f = open(name + "Pub.key", "r")
-keys[1] = RSA.importKey(f.read())
-f.close()
-print(keys)
+with open('tests/keys', 'r') as f:
+    keys = json.loads(f.readline())
+my_keys = keys[name]
+with open('tests/genblock.bl', 'r') as f:
+    bch[0] = block.Block.from_json(f.readline())
+loop = multiprocessing.Process(target=net.loop(), args=tuple())
+loop.start()
+loop.join()
+if name == 'Alice':
+    bch.new_transaction(my_keys[1], [0, 0], [keys['Bob']], [1], privkey=my_keys[0])
+time.sleep(2)
+if name == 'Bob':
+    bch.append(block.Block())
+print(name, bch[0].txs[1].__dict__)
+print(bch[1])
