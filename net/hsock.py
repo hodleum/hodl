84d7c54addeb5fd81b106c4734f9081f2852bfff
leofnch <leva.chizhow@gmail.com>
2018-03-22 17:31:51 +0300

    fix in net

--- net/hsock.py
+++ net/hsock.py
@@ -1,4 +1,6 @@
 from socket import socket
+import multiprocessing
+import json
 
 
 class HSock:
@@ -15,6 +17,11 @@ class HSock:
             # create connection (self.conn) between this device and white peer, which connects to this peer.
             # Generate RSA keys.
             self.white_peer = False
+        self.in_msgs = []
+        # start listen as daemon (using multiprocessing) and put messages to self.in_msgs
+        self.l = multiprocessing.Process(target=self.listen)
+        self.l.start()
+        self.l.join()
 
     def send(self, data):
         if self.white_peer:
@@ -22,3 +29,24 @@ class HSock:
         else:
             # encrypt with RSA and send using self.conn
             pass
+
+    def listen(self):
+        """
+        Listen for new messages and write it to self.in_msgs
+        :return:
+        """
+        data = b''
+        while True:
+            p = self.conn.recv(1024)
+            if not p:
+                self.in_msgs.append(data.decode())
+                data = b''
+            data += p
+
+    def close(self):
+        """
+        Close socket.
+        :return:
+        """
+        self.conn.close()
+        self.l.terminate()
