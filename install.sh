e592100fb20361edba007a30ceb37bffb835e567
leofnch <leva.chizhow@gmail.com>
2018-06-11 21:10:00 +0300

    Installation

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt-get install docker sqlite3
 sudo pip3 install docker pycrypto
 python3 setup.py install
 docker build -t hodl-container - < Dockerfile
+mkdir tmp
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='hodl',
+    name='hodleum',
     version='0.1',
     packages=[],
     url='github.com/hodleum',
