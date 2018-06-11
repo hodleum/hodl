46138ccf98f7bf7650db9f2de8f8ffb6a3d34188
leofnch <leva.chizhow@gmail.com>
2018-06-11 21:26:11 +0300

    Installation

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='hodleum',
     version='0.1',
     packages=[],
-    url='github.com/hodleum',
-    license='MIT',
+    url='https://github.com/hodleum/hodl',
+    license='Apache 2.0',
     author='hodleum',
-    author_email='',
+    author_email='hodleum@gmail.com',
     description='HODL is a cryptocurrency.'
 )
