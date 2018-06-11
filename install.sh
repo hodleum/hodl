e938b35c7ca0b77a16a54483473c8a79824f02c3
leofnch <leva.chizhow@gmail.com>
2018-06-11 21:11:09 +0300

    Installation

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt-get install docker sqlite3
+sudo apt install sqlite3
 sudo pip3 install docker pycrypto
 python3 setup.py install
 docker build -t hodl-container - < Dockerfile
