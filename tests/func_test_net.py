17b180caa738943b6e75ac57d9f9741d9a02258b
leofnch <leva.chizhow@gmail.com>
2018-06-17 13:35:20 +0300

    Fixes for func_test_net

--- tests/func_test_net.py
+++ tests/func_test_net.py
@@ -9,28 +9,29 @@ from net.Peers import Peer, Peers
 
 name = str(os.getenv('HODL_NAME'))
 log.basicConfig(filename=name+'.log', level=log.DEBUG)
+log.debug('\n\n\n\n\n\nstart\n---------\n')
 with open('tests/keys', 'r') as f:
     keys = json.loads(f.readline())
 my_keys = keys[name]
 if name == 'Alice':
-    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.2', 5000)]),
-                             Peer(keys['Chuck'][1], [('172.19.0.3', 5000)]),
-                             Peer(keys['Dave'][1], [('172.19.0.4', 5000)])])
+    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.3', 5000)]),
+                             Peer(keys['Chuck'][1], [('172.19.0.4', 5000)]),
+                             Peer(keys['Dave'][1], [('172.19.0.5', 5000)])])
     time.sleep(2)
 if name == 'Bob':
-    peers = Peers([Peer(keys['Alice'][1], [('172.19.0.1', 5000)]),
-                             Peer(keys['Chuck'][1], [('172.19.0.3', 5000)]),
-                             Peer(keys['Dave'][1], [('172.19.0.4', 5000)])])
+    peers = Peers([Peer(keys['Alice'][1], [('172.19.0.2', 5000)]),
+                             Peer(keys['Chuck'][1], [('172.19.0.4', 5000)]),
+                             Peer(keys['Dave'][1], [('172.19.0.5', 5000)])])
 if name == 'Chuck':
-    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.2', 5000)]),
-                             Peer(keys['Alice'][1], [('172.19.0.1', 5000)]),
-                             Peer(keys['Dave'][1], [('172.19.0.4', 5000)])])
+    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.3', 5000)]),
+                             Peer(keys['Alice'][1], [('172.19.0.2', 5000)]),
+                             Peer(keys['Dave'][1], [('172.19.0.5', 5000)])])
 if name == 'Dave':
-    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.2', 5000)]),
-                             Peer(keys['Chuck'][1], [('172.19.0.3', 5000)]),
-                             Peer(keys['Alice'][1], [('172.19.0.1', 5000)])])
+    peers = Peers([Peer(keys['Bob'][1], [('172.19.0.3', 5000)]),
+                             Peer(keys['Chuck'][1], [('172.19.0.4', 5000)]),
+                             Peer(keys['Alice'][1], [('172.19.0.2', 5000)])])
 port = 5000
-log.debug(os.popen('ifconfig').read())
+log.debug(os.popen('ifconfig|grep inet |grep addr').read())
 peers.update([keys['Alice']], log=log)
 if name == 'Bob':
     for i in range(20):
