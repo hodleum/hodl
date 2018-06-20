66fe23746d40efd681172bad245000e833f8cdb7
Artem B <budanov01@bk.ru>
2018-06-20 17:32:40 +0300

    crypto docstrings

--- cryptogr/__init__.py
+++ cryptogr/__init__.py
@@ -1,7 +1,7 @@
 """Cryptography module for HODL
 h(s:str) is a hash function (output: hash of this string(str))
-gen_keys() generates private and public keys(output:private key(bytes), public key(bytes))
-sign(plaintext:str, private_key:bytes) signs plaintext(output:sign(bytes))
-verify_sign(sign:bytes, plaintext:str, public_key:bytes) checks if sign is valid (output: is sign valid(bool))"""
+gen_keys() generates private and public keys(output:private key(str), public key(str))
+sign(plaintext:str, private_key:str) signs plaintext(output:sign(str))
+verify_sign(sign:str, plaintext:str, public_key:str) checks if sign is valid (output: is sign valid(bool))"""
 
 from .tools import *
--- cryptogr/tools.py
+++ cryptogr/tools.py
@@ -14,14 +14,27 @@ def h(s):
 
 
 def gen_keys():
-    """Generates keys"""
+    """
+    Generates keys
+    :return: (private key, public key)
+    """
     privatekey = RSA.generate(2048)
     publickey = privatekey.publickey()
     return privatekey.exportKey().decode(), publickey.exportKey().decode()
 
 
 def sign(plaintext, private_key):
-    """Creates signature"""
+    """
+    Creates signature
+
+    :param plaintext: text
+    :type plaintext: str
+
+    :param private_key: RSA private key
+    :type private_key: str
+
+    :return: str
+    """
     priv_key = RSA.importKey(private_key)
     plaintext = plaintext.encode('utf-8')
     # creation of signature
@@ -32,7 +45,20 @@ def sign(plaintext, private_key):
 
 
 def verify_sign(s, plaintext, public_key):
-    """Verifies signature"""
+    """
+    Verifies signature
+
+    :param s: signature
+    :type s: str
+
+    :param plaintext: text
+    :type plaintext: str
+
+    :param public_key: RSA public key
+    :type public_key: str
+
+    :return: bool
+    """
     pub_key = RSA.importKey(public_key)
     plaintext = plaintext.encode('utf-8')
     # decryption signature
@@ -43,6 +69,17 @@ def verify_sign(s, plaintext, public_key):
 
 
 def encrypt_aes(plaintext, key):
+    """
+    Encrypt text with AES cipher
+
+    :param plaintext: text to encrypt
+    :type plaintext: str
+
+    :param key: 16/32 bytes key
+    :type key: bytes
+
+    :return: str
+    """
     plaintext = struct.pack('>L', len(plaintext)) + plaintext.encode('utf-8') + b'\x00' * (
             16 - (len(plaintext) + 4) % 16)
     aes = AES.new(key)
@@ -50,6 +87,17 @@ def encrypt_aes(plaintext, key):
 
 
 def decrypt_aes(text, key):
+    """
+    Decrypt ciphertext with AES
+
+    :param text: ciphertext to decrypt
+    :type text: str
+
+    :param key: 16/32 bytes key
+    :type key: bytes
+
+    :return: str
+    """
     aes = AES.new(key)
     plaintext = aes.decrypt(base64.decodebytes(text.encode()))
     text_len = struct.unpack('>L', plaintext[:4])[0]
@@ -57,6 +105,17 @@ def decrypt_aes(text, key):
 
 
 def encrypt(plaintext, pub_key):
+    """
+    Encrypt text with RSA
+
+    :param plaintext: text to encrypt
+    :type plaintext: str
+
+    :param pub_key: RSA public key
+    :type pub_key: str
+
+    :return: str
+    """
     key = RSA.importKey(pub_key)
     session = get_random_bytes(32)
     enc_session = PKCS1_OAEP.new(key).encrypt(session)
@@ -65,6 +124,17 @@ def encrypt(plaintext, pub_key):
 
 
 def decrypt(text, priv_key):
+    """
+    Decrypt ciphertext with RSA
+
+    :param text: ciphertext to decrypt
+    :type text: str
+
+    :param priv_key: RSA private key
+    :type priv_key: str
+
+    :return: str
+    """
     key = RSA.importKey(priv_key)
     text = base64.decodebytes(text.encode())
     cipher_text = base64.encodebytes(text[(key.size() + 1) // 8:]).decode()
