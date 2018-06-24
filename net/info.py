bf5411cca161d066bce2d79ba6cb4608a16c73e0
DanGSun <dangorlov@yandex.ru>
2018-06-24 15:10:51 +0300

    Protocol Generate Function
    Signed-off-by: DanGSun <dangorlov@yandex.ru>

---
+++ net/info.py
@@ -0,0 +1,17 @@
+""" INFORMATION ABOUT HODL NETWORKING """
+
+PNAME="HODL"
+VERSION="v0.1-alpha"
+SUPPORTED_TYPES=[
+    "sync",
+    "PRequest",
+    "E2E-SmartContract",
+    "AvailabilityPing"
+]
+
+SUPPORTED_ENCODINGS=[
+    "json",
+    "base64",
+    "b+gzip",
+    "text"
+]
