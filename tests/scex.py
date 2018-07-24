383fb4683eece0cf3431adf3fd655d4d57fdce3d
ennucore <ennucore@gmail.com>
2018-07-24 15:16:07 +0100

    sc example

--- tests/scex.py
+++ tests/scex.py
@@ -3,28 +3,14 @@ import json
 import block
 import cryptogr as cg
 
-mem = s.get_self().memory
-try:
-    balances, bc, tc = json.loads(mem.local)   # bc - number of last processed block
-    bc = int(bc)
-    start = False
-except ValueError:
-    balances, bc, tc = {}, 0, 0
-    start = True
+
+balances, bc, tc = {}, 0, 0
+start = True
 
 
 def add_task(sender, task):
     if sender == s.get_self().author:
         s.append_tasks(task)
-
-
-def write():
-    with open('tmp/sc.mem', 'w') as f:
-        memory = s.get_self().memory
-        bc = len(s.bch)
-        memory.clear()
-        memory += json.dumps((balances, len(s.bch), len(s.bch[-1].txs)))
-        f.write(str(memory))
 
 
 def send(sender, money, to):
@@ -42,21 +28,23 @@ def sell(sender, money):
             write()
 
 
-if not start:
-    if tc != len(s.bch[bc-1].txs):
-        for tnx in s.bch[bc-1].txs[tc:len(s.bch[bc-1].txs)]:
-            if 'sc' + str(ind) in tnx.outs:
-                try:
-                    balances[tnx.author] += tnx.outns[tnx.outs.index('sc' + str(ind))]
-                except:
-                    balances[tnx.author] = tnx.outns[tnx.outs.index('sc' + str(ind))]
-if bc != len(s.bch):
-    for i in range(bc, len(s.bch)):
-        for tnx in s.bch[i].txs:
-            if 'sc' + str(ind) in tnx.outs:
-                try:
-                    balances[tnx.author] += tnx.outns[tnx.outs.index('sc' + str(ind))]
-                except:
-                    balances[tnx.author] = tnx.outns[tnx.outs.index('sc' + str(ind))]
 balances['0'] = 0.2
-write()
+while True:
+    if not start:
+        if tc != len(s.bch[bc-1].txs):
+            for tnx in s.bch[bc-1].txs[tc:len(s.bch[bc-1].txs)]:
+                if 'sc' + str(ind) in tnx.outs:
+                    try:
+                        balances[tnx.author] += tnx.outns[tnx.outs.index('sc' + str(ind))]
+                    except:
+                        balances[tnx.author] = tnx.outns[tnx.outs.index('sc' + str(ind))]
+    if start:
+        start = False
+    if bc != len(s.bch):
+        for i in range(bc, len(s.bch)):
+            for tnx in s.bch[i].txs:
+                if 'sc' + str(ind) in tnx.outs:
+                    try:
+                        balances[tnx.author] += tnx.outns[tnx.outs.index('sc' + str(ind))]
+                    except:
+                        balances[tnx.author] = tnx.outns[tnx.outs.index('sc' + str(ind))]
