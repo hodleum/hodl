b51b17177bdfa1480f1a8046d35129b0cec7bdab
ennucore <ennucore@gmail.com>
2018-08-13 19:18:04 +0100

    smart contract memory

--- block/sc/machine.py
+++ block/sc/machine.py
@@ -1,28 +1,7 @@
 """
 VPy config
 """
-from vpy.fs import FS
 from vpy.proc import Proc
-
-
-class DFS(FS):
-    """
-    Decentralized FileSystem - class for storing filesystem and global vars for VPy
-    """
-    def __getitem__(self, item):
-        pass   # todo
-
-    def __setitem__(self, key, value):
-        pass   # todo
-
-    def __delitem__(self, key):
-        pass   # todo
-
-    def __len__(self):
-        pass   # todo
-
-    def add(self, data):
-        pass   # todo
 
 
 class DProc(Proc):
--- block/sc/sc.py
+++ block/sc/sc.py
@@ -2,23 +2,19 @@ import json
 import time
 import os
 import logging as log
+import vpy
 import cryptogr as cg
+from block.sc.memory import SCMemory, sc_base_mem
 
 
-sc_base_mem = 10000000
 sc_base_code_size = 5000000
 sc_memprice = 0.001
 sc_code_price = 10**(-6)
 sc_price = 0
-one_peer_max_mem = 4000000
 sc_award_from = 1
 sc_award_to = 5
 # todo: remove tasks from smart contracts, create pool of calculating tasks with difficulty mark
 # todo: smart contract's signs
-
-
-class SCMemoryError(Exception):
-    pass
 
 
 def check_sc_award_tnx(bch, tnxind, sc):
@@ -58,6 +54,7 @@ class SmartContract:
         self.awards = {}
         self.sign = ''
         self.memory_distribution = []   # [[Miners for part1]]
+        self.signs = []
 
     def sign_sc(self, privkey):
         self.sign = cg.sign(self.sign_str(), privkey)
@@ -70,7 +67,7 @@ class SmartContract:
         :param args: func's args
         :param is_task: if executing task, no timeout needed
         """
-        # todo: allow SC to confirm its memory, not to calculate it every time
+        # todo: run with vpy
         file = open('tmp/__init__.py', 'w')
         file.close()
         file = open('sc_main.py', 'w')
@@ -241,97 +238,3 @@ class SmartContract:
         # todo: distribute miners if needed
         self.memory.distribute_peers()
         self.distribute_tasks()
-
-
-class SCMemory:
-    def __init__(self, sc, size):
-        self.scind = sc
-        self.size = size
-        self.local = ''
-        self.localind = [0, 0]
-        self.length = 0
-        self.peers = []
-        self.accepts = []   # [{miner1:[cg.h(mem, miner1), [acceptions or declinations for this user(a/d, sign, address)]]} for part in memory]
-
-    def __getitem__(self, item):
-        if item.start < 0:
-            item.start = len(self) - item.start
-        if item.stop < 0:
-            item.stop = len(self) - item.stop
-        if self.size > sc_base_mem:
-            if item.start > self.localind[0] and item.stop < self.localind[1]:
-                return get_sc_memory(self.scind, item.start, item.stop)
-        else:
-            return self.local[item]
-
-    def __setitem__(self, key, value):
-        if type(key) == slice:
-            if key.start >= self.localind[0] and key.stop - self.localind[0] <= sc_base_mem:
-                self.local[key.start - self.localind[0]:key.stop - self.localind[0]] = value
-            elif self.localind[1] > key.start >= self.localind[0]:
-                if key.stop <= self.localind[1]:
-                    self.local[key.start - self.localind[0]:key.stop - self.localind[0]] = value
-                else:
-                    self.local[key.start - self.localind[0]:self.localind[1]] = value
-            elif key.start <= self.localind[0] and self.localind[1] >= key.stop >= self.localind[0]:
-                self.local[0:key.stop - self.localind[0]] = value
-        else:
-            if self.localind[1] > key > self.localind[0]:
-                self.local = list(self.local)
-                self.local[key] = value
-                self.local = ''.join(self.local)
-
-    def __add__(self, other):
-        if len(self) + len(other) > self.size:
-            raise SCMemoryError
-        if len(self) + len(other) < sc_base_mem:
-            self.localind[1] = len(self) + len(other)
-            self.local += other
-        self.length = len(self) + len(other)
-        return self
-
-    def distribute_peers(self):
-        """
-        Distribute memory between miners
-        :return:
-        """
-        self.peers.sort()
-        l = len(self)
-        m = len(self.peers)
-        opm = ((one_peer_max_mem * m)//(l))
-        n = (l//one_peer_max_mem) + 1
-        if self.size <= sc_base_mem:
-            self.accepts = []
-        else:
-            self.accepts = [{p: ['', []] for p in self.peers[i*opm:(i+1)*opm]} for i in range(n)]
-
-    def __len__(self):
-        return self.length
-
-    def __str__(self):
-        """
-        Save memory to str so it can be restored
-        :return:
-        """
-        return json.dumps([self.scind, self.size, self.local, self.localind, self.length, self.peers, self.accepts])
-
-    @classmethod
-    def from_json(cls, s):
-        """
-        Restore memory from str
-        :param s: str
-        :return: SCMemory
-        """
-        l = json.loads(s)
-        self = cls(l[0], l[1])
-        self.local = l[2]
-        self.localind = l[3]
-        self.length = l[4]
-        self.peers = l[5]
-        self.accepts = l[6]
-        return self
-
-    def clear(self):
-        self.length = 0
-        self.local = ''
-        self.localind = [0, 0]
