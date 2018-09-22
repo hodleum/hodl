cd4fd9e01cb4f2d133dd43773b08d108869ffc06
ennucore <ennucore@gmail.com>
2018-09-22 17:08:07 +0300

    TODOs,docs

--- sync/handle.py
+++ sync/handle.py
@@ -11,7 +11,6 @@ Bob(b) sends len(B.bch), len(B.bch[-1].txs), len(B.bch[-1].contracts), if len(B.
 If Alice's blockchain is shorter, A sends request,
 if len(A.bch) == len(B.bch), if lengths of Alice's lists of bch[-1]'s txs or contracts are shorter, A sends request
 """
-import json
 import block
 from net2.protocol import server
 from net2.models import Message
@@ -23,10 +22,13 @@ syncs = []
 
 
 class SyncHandler:
+    """
+    Class for synchronization conversation with one peer
+    """
     def __init__(self, address, msg=None, u=None):
         self.address = address
         self.msgs = []
-        # todo
+        # todo: realize algorythm above
         if not msg:
             answer = dict()
             u.send(Message('sync', answer))
@@ -34,17 +36,20 @@ class SyncHandler:
             u.send(self.on_message(msg))
 
     def on_message(self, msg):
-        answer = []
+        answer = dict()
         self.msgs.append(msg)
-        # todo
+        # todo: realize algorythm above
+        # todo: if synchronization complete, delete history
         return answer
 
 
 @server.handle('sync')
 def handle_msg(msg):
     h = {u.address: u for u in syncs}.get(user.pub_key)
+    # if we have sync history with this user, use it
     if h:
         user.send(Message('sync', h.on_message(msg)))
+    # if we receive message from this user at first time, create conversation
     else:
         h = SyncHandler(user.pub_key, msg, user)
         user.send(Message('sync', h.on_message(msg)))
