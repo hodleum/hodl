01f8171e9d766dd347e19a550ece48ffc0809c9e
ennucore <ennucore@gmail.com>
2018-09-23 15:02:19 +0300

    test_sync

---
+++ run_net_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker stop Alice Bob Chuck Dave
+docker rm Alice Bob Chuck Dave
+docker network rm hodlnet
+./net_test.sh
+sleep 15
+docker container logs Alice
+docker container logs Bob
+docker container logs Chuck
+docker container logs Dave
--- run_sync_test.sh
+++ run_sync_test.sh
@@ -2,8 +2,8 @@
 docker stop Alice Bob Chuck Dave
 docker rm Alice Bob Chuck Dave
 docker network rm hodlnet
-./net_test.sh
-sleep 20
+./test_sync.sh
+sleep 15
 docker container logs Alice
 docker container logs Bob
 docker container logs Chuck
