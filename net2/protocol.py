71486acd65e6611da74ba5f5ba392e6166d1276b
Artem B <budanov01@bk.ru>
2018-10-01 14:33:19 +0300

    Net2: send, forward, shout, random_send, TODOs

--- net2/protocol.py
+++ net2/protocol.py
@@ -1,7 +1,7 @@
 from .config import *
 from .models import *
 from .server import peer, protocol, server
-import random
+
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO,
@@ -35,7 +35,7 @@ def share_users():
 def record_new_user(key, name):
     new_user = session.query(User).filter_by(name=name)
     if not new_user:
-        new_user = User(protocol, pub_key=key, name=name)
+        new_user = User(protocol, public_key=key, name=name)
         protocol.add_object(new_user)
         protocol.send_all(Message(
             name='new_user',
@@ -48,7 +48,7 @@ def record_peers(peers):
     with lock:
         for data in peers:
             if not session.query(Peer).filter_by(addr=data['address']):
-                new_peer = Peer(protocol, pub_key=data['key'], addr=data['address'])
+                new_peer = Peer(protocol, public_key=data['key'], addr=data['address'])
                 session.add(new_peer)  # TODO: test new peers
         session.commit()
 
@@ -58,23 +58,9 @@ def record_users(users):
     with lock:
         for data in users:
             if not session.query(User).filter_by(name=data['name']):
-                new_user = Peer(protocol, pub_key=data['key'], name=data['name'])
+                new_user = Peer(protocol, public_key=data['key'], name=data['name'])
                 session.add(new_user)
         session.commit()
-
-
-@server.handle('forward', 'request')  # TODO: with_wrapper=True
-def forward(message):
-    if random.randint(0, 3) == random.randint(0, 3):
-        protocol.send_all(Message(
-            name='message',
-            data=message
-        ))
-    else:
-        protocol.random_send(Message(
-            name='forward',
-            data=message
-        ))
 
 
 if __name__ == '__main__':
