c9614b65583728a7905c001b422e1cb74302a581
ennucore <ennucore@gmail.com>
2018-11-01 14:32:58 +0300

    tests

---
+++ run_twnc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+docker stop Alice Bob Chuck Dave miner evil_miner
+docker rm Alice Bob Chuck Dave miner evil_miner
+docker network rm hodlnet
+./twnc.sh
+sleep 15
+echo "Alice:"
+docker container logs Alice
+echo "Bob:"
+docker container logs Bob
+echo "Chuck:"
+docker container logs Chuck
+echo "Dave:"
+docker container logs Dave
+echo "miner:"
+docker container logs miner
+echo "evil_miner:"
+docker container logs evil_miner
+
